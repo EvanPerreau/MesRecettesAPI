54e42a93e15c35146d9c8aebaff4de28ca1ffe25
Evan <evan.perreau@gmail.com>
2024-04-16 19:31:32 +0200

    init commit

---
+++ src/main/java/fr/evanperreau/MesRecettesAPI/MesRecettesApiApplication.java
@@ -0,0 +1,13 @@
+package fr.evanperreau.MesRecettesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesRecettesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesRecettesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/evanperreau/MesRecettesAPI/MesRecettesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.evanperreau.MesRecettesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesRecettesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
